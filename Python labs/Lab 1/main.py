7c934fe30314dcad61a74cecca4dde9375eb6a5b
IhtiyarT <turdievihtiyar@gmail.com>
2024-09-03 21:40:45 +0300

    1 lab proc, first vers

---
+++ Python labs/Lab 1/main.py
@@ -0,0 +1,47 @@
+from math import sqrt
+from sys import argv
+
+
+def get_coef(ind: int, promt: str) -> float:
+    try:
+        coef = argv[ind]
+    except IndexError:
+        coef = input(promt)
+    try:
+        ret_coef = float(coef)
+        return ret_coef
+    except ValueError:
+        return get_coef(ind, promt)
+
+
+def get_result(a: float, b: float, c: float) -> set:
+
+    result = set()
+    try:
+        sdisc = sqrt(b * b - 4 * a * c)
+
+        x1 = (-b + sdisc) / (2 * a)
+        result.add(sqrt(x1))
+        result.add(-sqrt(x1))
+
+        x2 = (-b - sdisc) / (2 * a)
+        result.add(sqrt(x2))
+        result.add(-sqrt(x2))
+        return result
+    except ValueError or TypeError:
+        return result
+
+
+def main():
+    a = get_coef(1, "Enter coefficient a: ")
+    b = get_coef(2, "Enter coefficient b: ")
+    c = get_coef(3, "Enter coefficient c: ")
+    result = get_result(a, b, c)
+    if result:
+        print(result)
+    else:
+        print("No roots found")
+
+
+if __name__ == '__main__':
+    main()
