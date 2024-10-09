90720c88a511a40e723494d511c603dc1d31304e
IhtiyarT <turdievihtiyar@gmail.com>
2024-10-09 22:30:08 +0300

    rk1 first try

---
+++ RK1/main.py
@@ -0,0 +1,87 @@
+from operator import itemgetter
+
+
+class ProgramLang:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+class Operator:
+    def __init__(self, id, name, ascii_code,prog_lang_id):
+        self.id = id
+        self.name = name
+        self.ascii_code = ascii_code
+        self.prog_lang_id = prog_lang_id
+
+
+class ProgLangOp:
+    def __init__(self, prog_lang_id, op_id):
+        self.prog_lang_id = prog_lang_id
+        self.op_id = op_id
+
+
+prog_langs = [
+    ProgramLang(1, "C++"),
+    ProgramLang(2, "Java"),
+    ProgramLang(3, "Python"),
+]
+
+ops = [
+    Operator(1, "Plus", 43,1),
+    Operator(2, "Minus", 45,2),
+    Operator(3, "Equal", 61,3),
+    Operator(4, "Ampersand", 38, 3),
+    Operator(5, "Divide", 47, 1),
+    Operator(5, "Multiply", 42, 1)
+]
+
+pl_ops = [
+    ProgLangOp(1, 1),
+    ProgLangOp(2, 2),
+    ProgLangOp(3, 3),
+    ProgLangOp(3, 4),
+    ProgLangOp(1, 5),
+]
+
+
+def main():
+    one_to_many = [(op.name, op.ascii_code, pl.name)
+                   for pl in prog_langs
+                   for op in ops
+                   if op.prog_lang_id == pl.id]
+
+    many_to_many_temp = [(pl.name, ps.prog_lang_id, ps.op_id)
+                         for pl in prog_langs
+                         for ps in pl_ops
+                         if ps.prog_lang_id == pl.id]
+
+    many_to_many = [(op.name, op.ascii_code, pl_name)
+                    for pl_name, pl_id, op_id in many_to_many_temp
+                    for op in ops if op.id == op_id]
+
+    print('Задание Б1')
+    res_1 = sorted(one_to_many, key=itemgetter(0))
+    print(res_1)
+
+    print("\nЗадание Б2")
+    res_2 = []
+    temp_dict = dict()
+    for i in one_to_many:
+        if i[2] in temp_dict:
+            temp_dict[i[2]] += 1
+        else:
+            temp_dict[i[2]] = 1
+    for i in temp_dict.keys():
+        res_2.append((i, temp_dict[i]))
+
+    res_2.sort(key=itemgetter(1), reverse=True)
+    print(res_2)
+
+    print("\nЗадание Б3")
+    res_3 = [(i[0], i[2]) for i in many_to_many if i[0].endswith('s')]
+    print(res_3)
+
+
+if __name__ == '__main__':
+    main()
